588fc55491889d46a3a3f951e294c8dc77761cac
ahmed-saeed-ivolve <59389612+ahmed-saeed-ivolve@users.noreply.github.com>
2020-03-11 17:50:15 +0500

    Update run-with-kube.sh

--- run-with-kube.sh
+++ run-with-kube.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl create -f middleware-service.yaml,middleware-deployment.yaml,iam-deployment.yaml,iam-service.yaml,python-app-service.yaml,python-app-deployment.yaml,webclient-deployment.yaml,webclient-service.yaml
+kubectl create -f ecs-monitoring-deployment.yaml,ecs-monitoring-service.yaml,ms-logger-service.yaml,ms-logger-deployment.yaml,webclient-deployment.yaml,webclient-service.yaml,kms-deployment.yaml,kms-service.yaml,reserved-ecs-deployment.yaml,reserved-ecs-service.yaml,python-app-deployment.yaml,python-app-service.yaml,emailer-deployment.yaml,emailer-service.yaml,middleware-deployment.yaml,middleware-service.yaml,usage-report-service.yaml,usage-report-deployment.yaml,iam-deployment.yaml,iam-service.yaml,report-collector-service.yaml,report-collector-deployment.yaml
