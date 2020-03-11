b3201142fa38fb92412d476174cecacddd177d6d
ahmed-saeed-ivolve <59389612+ahmed-saeed-ivolve@users.noreply.github.com>
2020-03-11 17:52:47 +0500

    Update down-envirment.sh

--- down-envirment.sh
+++ down-envirment.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-kubectl delete deployment --all
-kubectl delete svc --all
-kubectl delete pods --all
+kubectl delete -f ecs-monitoring-deployment.yaml,ecs-monitoring-service.yaml,ms-logger-service.yaml,ms-logger-deployment.yaml,webclient-deployment.yaml,webclient-service.yaml,kms-deployment.yaml,kms-service.yaml,reserved-ecs-deployment.yaml,reserved-ecs-service.yaml,python-app-deployment.yaml,python-app-service.yaml,emailer-deployment.yaml,emailer-service.yaml,middleware-deployment.yaml,middleware-service.yaml,usage-report-service.yaml,usage-report-deployment.yaml,iam-deployment.yaml,iam-service.yaml,report-collector-service.yaml,report-collector-deployment.yaml
